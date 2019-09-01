fa5da8de9642d0bc2b78aa18058af290ebe366a4
jordz99 <51729051+jordz99@users.noreply.github.com>
2019-09-01 20:24:15 +1000

    UPDA

--- main.js
+++ main.js
@@ -5,41 +5,34 @@ $(".menu").click(function() {
 
 $(document).ready(function() {
 
-    // gallery data potentially arriving from an AJAX call...
-    var imageLibraryData = [
-        "gallery/one.jpg",
-        "gallery/two.jpg",
-        "gallery/three.jpg",
-        "gallery/four.jpg",
-        "gallery/five.jpg",
-        "gallery/six.jpg",
-        "gallery/seven.jpg",
-        "gallery/eight.jpg",
-        "gallery/nine.jpg",
-        "gallery/ten.jpg",
-        "gallery/eleven.jpg",
-        "gallery/twelve.jpg"
-        ];
+  var imageLibraryData = [
+    "gallery/one.jpg",
+    "gallery/two.jpg",
+    "gallery/three.jpg",
+    "gallery/four.jpg",
+    "gallery/five.jpg",
+    "gallery/six.jpg",
+    "gallery/seven.jpg",
+    "gallery/eight.jpg",
+    "gallery/nine.jpg",
+    "gallery/ten.jpg",
+    "gallery/eleven.jpg",
+    "gallery/twelve.jpg"
+    ];
 
     var currentImageIndex = 0;
     var imageWidth = 306;
     var imageLibraryCount = imageLibraryData.length;
     var imageStripWidth = imageWidth * imageLibraryCount;
 
-    // setup width of gallery image strip
-    //
     $('#gallery_image')
         .css('width', imageStripWidth)
         .css('min-width', imageStripWidth);
 
-    // load images and tile them.
-    //
     for (var i = 0; i < imageLibraryCount; i++) {
         $('#gallery_image').append('<img class="tiled_image" src="' + imageLibraryData[i] + '" />');
     }
 
-    // setup jQuery events for button clicks
-    //
     $('#next').click(function() {
         currentImageIndex++;
         if (currentImageIndex > imageLibraryCount - 1) {
@@ -48,6 +41,7 @@ $(document).ready(function() {
             $("#gallery_image").animate({"left": "-=" + imageWidth}, "slow");
         }
     });
+
     $('#previous').click(function() {
         currentImageIndex--;
         if (currentImageIndex < 0) {
@@ -82,10 +76,6 @@ $("#previous").on("mouseleave", function(){
   "background-color": "black",
   "border": "solid 1px #ff6600"});
 });
-
-
-
-
 
 $("#next").on("mouseenter", function(){
   $(this).css({
